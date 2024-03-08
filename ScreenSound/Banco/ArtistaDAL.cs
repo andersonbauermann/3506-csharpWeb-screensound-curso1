a6ade15eda18f0001462d2c3fc8b5048caae3376
Anderson Bauermann Feltes <anderson.bauermannfeltes@gmail.com>
2024-03-08 08:19:50 -0300

    Implementado o EF ao projeto
    Implementado o entity framework ao projeto para persistir dados na tabela de artistas

---
+++ ScreenSound/Banco/ArtistaDAL.cs
@@ -0,0 +1,42 @@
+﻿using ScreenSound.Modelos;
+
+namespace ScreenSound.Banco
+{
+    internal class ArtistaDAL
+    {
+        private readonly ScreenSoundContext context;
+
+        public ArtistaDAL(ScreenSoundContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Artista> Listar()
+        {
+            return context.Artistas.ToList();
+        }
+
+        public Artista? BuscaPeloNome(string nome)
+        {
+            return context.Artistas.FirstOrDefault(artista => artista.Nome.Equals(nome));
+        }
+
+        public void Adicionar(Artista artista)
+        {
+            context.Artistas.Add(artista);
+            context.SaveChanges();
+        }
+
+        public void Atualizar(Artista artista)
+        {
+            context.Artistas.Update(artista);
+            context.SaveChanges();
+        }
+
+        public void Deletar(Artista artista)
+        {
+            context.Artistas.Remove(artista);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ ScreenSound/Banco/ScreenSoundContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Banco;
+internal class ScreenSoundContext : DbContext
+{
+    public DbSet<Artista> Artistas { get; set; }
+
+    private string connectionString = "";
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+}
